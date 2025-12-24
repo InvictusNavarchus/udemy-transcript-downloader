be1a3a1a6ce88e73e2ae6cfbac98a1279b479a87
Invictus Navarchus <github.com.compile095@passmail.com>
2025-12-24 11:04:48 +0700

    chore: set manifest config

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,11 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     srcDir: 'src',
+    manifest: {
+        permissions: ['storage', 'activeTab'],
+        host_permissions: ['https://www.udemy.com/*'],
+        action: {
+            default_popup: 'entrypoints/popup/index.html',
+     },
+  },
 });
