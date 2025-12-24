ea132cff5e6628a66d102e77b94789cc7063209c
Invictus Navarchus <github.com.compile095@passmail.com>
2025-12-24 11:17:52 +0700

    feat: add and enable @wxt-dev/auto-icons module

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     srcDir: 'src',
+    modules: ['@wxt-dev/auto-icons'],
     manifest: {
         permissions: ['storage', 'activeTab'],
         host_permissions: ['https://www.udemy.com/*'],
