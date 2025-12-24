bdad615e74107a0802888932dd09769296232162
Invictus Navarchus <github.com.compile095@passmail.com>
2025-12-24 11:07:12 +0700

    fix: resolve blob type error with Uint8Array conversion

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -224,7 +224,7 @@ export default defineContentScript({
         }
 
         // Trigger Download
-        const blob = new Blob([data], { type: 'application/zip' });
+        const blob = new Blob([new Uint8Array(data)], { type: 'application/zip' });
         const url = URL.createObjectURL(blob);
         const a = document.createElement('a');
         a.href = url;
